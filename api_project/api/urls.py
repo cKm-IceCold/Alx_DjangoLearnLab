324f2230137966cfdca47aeae769f9595abd5667
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-11-19 18:44:26 +0100

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -10,4 +11,5 @@ urlpatterns = [
     #path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
    
     path('', include(router.urls)),
+    path('auth/token/', obtain_auth_token, name='api_token_auth')
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
- 
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly 
 
 # Create your views here.
 #class BookList(ListAPIView):
@@ -14,5 +15,20 @@ from rest_framework import viewsets
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+ #Implementing permissions in view.py
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class SecureBookViewSet(viewsets.ModelViewSet):
+   queryset = Book.objects.all()
+   serializer_class = BookSerializer
+
+   permission_classes = [IsAuthenticated, IsAdminUser]
+
+class PublicReadOnlyBookViewSet(viewsets.ModelViewSet):
+   queryset = Book.objects.all()
+   serializer_class = BookSerializer
+   
 
 
